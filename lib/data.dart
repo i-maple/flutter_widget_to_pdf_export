26d767d9a7b00d69346948612fde9d1e2154f3d5
Nishant <nishantyt0@gmail.com>
2024-01-11 10:26:47 +0545

    excel api implemented

---
+++ lib/data.dart
@@ -0,0 +1,26 @@
+List<List> tableData = [
+  [
+    "Nishant Pokhrel",
+    "20",
+    "Nepal",
+    "Flutter",
+  ],
+  [
+    "Sagun Khatri",
+    "20",
+    "Nepal",
+    "Node.js",
+  ],
+  [
+    "Peter Trego",
+    "21",
+    "USA",
+    "React.js",
+  ],
+  [
+    "Dhanush Krishnamurthy",
+    "22",
+    "India",
+    "Flutter",
+  ]
+];
---
+++ lib/excel_api.dart
@@ -0,0 +1,55 @@
+import 'package:excel/excel.dart';
+import 'package:excel_export/data.dart';
+
+class ExcelAPI {
+  void fillExcelSheet() {
+    var excel = Excel.createExcel();
+    Sheet sheetObject = excel['demoSheet'];
+
+    CellInfo cellInfo = CellInfo();
+
+    for (var rows in tableData) {
+      
+      for (var items in rows) {
+        var cell = sheetObject.cell(
+          CellIndex.indexByString(cellInfo.toString()),
+        );
+        cell.value = TextCellValue(items);
+        cell.cellStyle = CellStyle();
+        cellInfo.incrementCellLetter();
+      }
+      cellInfo.resetLetter();
+      cellInfo.incrementCellNumber();
+    }
+
+    excel.save(
+      fileName: 'example_excel.xlsx',
+    );
+  }
+}
+
+class CellInfo {
+  String cellLetter;
+  int cellNumber;
+
+  CellInfo({this.cellLetter = 'A', this.cellNumber = 1});
+
+  @override
+  String toString() => '$cellLetter${cellNumber.toString()}';
+
+  incrementCellNumber() {
+    cellNumber = cellNumber + 1;
+  }
+
+  void resetNumber(){
+    cellNumber = 1;
+  }
+  void resetLetter(){
+    cellLetter = 'A';
+  }
+  
+
+  incrementCellLetter() {
+    cellLetter = String.fromCharCode(cellLetter.codeUnitAt(0) + 1);
+  }
+}
